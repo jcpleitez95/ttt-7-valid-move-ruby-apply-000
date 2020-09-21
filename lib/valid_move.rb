08bd4072b5b2a4eded2fe1b1cb8589468e7ef64f
essential-blog-3393 <jcpleitez@outlook.com>
2020-09-21 16:56:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,10 @@
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+    return false 
+  else 
+    return true 
+  end
+end 
 
 def valid_move?(board, idex)
   if position_taken?
@@ -6,6 +13,3 @@ def valid_move?(board, idex)
     return true 
   end
 end
-
-
-
